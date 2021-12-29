8131e7544f7e6bb48a0d28c96476bbe5b8824026
Daniel Jiang <jiang.s.daniel@gmail.com>
2021-12-29 01:29:57 -0800

    Finished registration page and login page

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -68,7 +68,7 @@ module.exports = {
             if (user) {
                 throw new UserInputError('Username is taken', {
                     errors: {
-                        username: 'This username is taken'
+                        username: 'Username is taken'
                     }
                 })
             }
