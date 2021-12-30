fd216b1adb9ebda031ee3170719b54ed90a1be06
Daniel Jiang <jiang.s.daniel@gmail.com>
2021-12-30 01:15:47 -0800

    Users can now create and add posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import { Feed, Grid } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 
+import { AuthContext } from '../context/auth.js'
 import PostCard from '../components/PostCard.js'
+import PostForm from '../components/PostForm.js'
+import { FETCH_POSTS_QUERY } from '../util/graphql.js'
 
 function Home() {
+
+    const { user } = useContext(AuthContext)
+
     const { loading, data } = useQuery(FETCH_POSTS_QUERY)
 
     let posts = <h1>Loading posts...</h1>
@@ -17,38 +22,25 @@ function Home() {
         ))
     }
 
+    let postForm = null
+    if (user) {
+        postForm = 
+        <Grid.Column>
+            <PostForm/>
+        </Grid.Column>
+    }
+
     return (
         <Grid columns={3}>
             <Grid.Row className='home-title'>
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
+                {postForm}
                 {posts}
             </Grid.Row>
         </Grid>
     )
 }
-
-const FETCH_POSTS_QUERY = gql`
-    {
-        getPosts {
-            id
-            body
-            createdAt
-            username
-            likeCount
-            likes {
-                username
-            }
-            commentCount
-            comments {
-                id
-                username
-                createdAt
-                body
-            }
-        }
-    }
-`
 
 export default Home
